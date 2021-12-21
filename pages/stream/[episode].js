9bd5c2edef59e1392f4d74f7bb3a66fdcaaa0b80
Marek Veselý <veselym@spstrutnov.cz>
2021-12-21 21:32:46 +0100

    Watch page

--- pages/stream/[episode].js
+++ pages/stream/[episode].js
@@ -1,18 +1,75 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import useSWR from "swr";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Recent.module.css";
 const fetcher = (url) => fetch(url).then((res) => res.json());
-const Post = () => {
+
+export default function Stream() {
     const router = useRouter();
+    const [ stream, setStream ] = useState("");
+    const [ streams, setStreams ] = useState([]);
     const { episode } = router.query;
     const { data, error } = useSWR(`/api/stream?episode=${episode}`, fetcher);
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    let page = [<h1>Hello world</h1>];
-    console.log(data)
-    for (const strm of data.servers) {
-        page.push(<iframe src={strm.iframe} allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>);
+    
+    if (stream === "") {
+        let strms = []
+        for (const s of data.servers) { 
+            strms.push(
+                <option value={s.iframe}>{s.name}</option>
+            )
+        }
+        setStreams(strms);
+        setStream(data.servers[0].iframe);
     }
-    return page;
-};
 
-export default Post;
+    const changeStream = (e) => {
+        setStream(e.target.value);
+    }
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Next-time</title>
+                <meta name="description" content="I am gonna kill myself" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to{" "}
+                    <a id="nt" href="https://github.com/notmarek/next-time">
+                        Next-time!
+                    </a>
+                </h1>
+
+                <select onChange={(event) => changeStream(event)} className={styles.select}>
+                    {streams}
+                </select>
+                
+                <div className={styles.grid}>
+                   <iframe src={stream} style={{width: "1080px", height: "720px"}} allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://gogoanime.cm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{" "}
+                    <span className={styles.logo}>
+                        <Image
+                            src="/gogo.png"
+                            alt="Gogoanime Logo"
+                            width={72}
+                            height={16}
+                        />
+                    </span>
+                </a>
+            </footer>
+        </div>
+    );
+}
